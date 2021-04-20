ea41aa2a505a7df8faae7476cf48ceabb637245a
DianeLagadec <82271409+DianeLagadec@users.noreply.github.com>
2021-04-20 10:06:18 +0200

    BDD
    connection a la BDD

--- src/restaurant/BDD.java
+++ src/restaurant/BDD.java
@@ -1,5 +1,31 @@
 package restaurant;
 
+import java.sql.*;
+import java.util.EmptyStackException;
+
 public class BDD {
-	//hello
+
+    public BDD() {
+    }
+    
+    public static Connection getConnection() {
+    	
+        try {
+            String protocole = "jdbc:mysql:";
+            String ip = "localhost";
+            String port = "3306";
+            String bddName = "Restaurant";
+            String connection = protocole + "//" + ip + ":" + port + "/" + bddName;
+            String userName = "root";
+            String password = "root";
+
+            Connection con = DriverManager.getConnection(connection, userName, password);
+
+            return con;
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        throw new EmptyStackException();
+    }    
 }
+// java -cp mysql-connector-java-8.0.23.jar:.: BDD.java 
